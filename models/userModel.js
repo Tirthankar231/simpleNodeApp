92972c3de84dbb4f91ef07730f6a7373e80c3b2e
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-04-16 11:01:53 +0530

    ADD : Added User Model

---
+++ models/userModel.js
@@ -0,0 +1,12 @@
+// models/user.js
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
