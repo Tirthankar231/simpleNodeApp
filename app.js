ab4729773490eae957d8e02e25af6ea104e28ce4
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-04-16 19:24:11 +0530

    ADD : Added authRoutes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import orderRoutes from '../simpleNodeApp/routes/orderRoute.js';
 import returnRoutes from '../simpleNodeApp/routes/returnRoute.js';
-import invoiceRoutes from '../simpleNodeApp/routes/invoiceRoute.js'
+import invoiceRoutes from '../simpleNodeApp/routes/invoiceRoute.js';
+import authRoutes from '../simpleNodeApp/routes/authRoute.js';
 import dotenv from 'dotenv';
 
 const app = express();
@@ -24,6 +25,7 @@ mongoose.connect(process.env.MONGODB_URL, {
 app.use('/api', orderRoutes)
 app.use('/api', returnRoutes)
 app.use('/api', invoiceRoutes)
+app.use('/api', authRoutes)
 
 // Start the server
 const PORT = process.env.PORT || 3000;
