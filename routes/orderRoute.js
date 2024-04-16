d3237ccb9cec25604f6b32c4de543a10ca9ddcc3
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-04-16 18:30:17 +0530

    ADD : Added listOrders and getOrderById routes to order route

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -10,4 +10,10 @@ router.post('/orders', orderController.createOrder);
 //Update a particular order with the help of its id
 router.put('/orders/:id', orderController.updateOrder);
 
+// GET request to list orders with optional filters
+router.get('/orders', orderController.listOrders);
+
+// GET request to get an order by ID
+router.get('/orders/:id', orderController.getOrderById);
+
 export default router;
